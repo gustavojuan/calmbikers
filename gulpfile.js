a4c4dffe44f62989dea91f37040a543475a2f99a
gustavojuan <gustavojuan@gmail.com>
2017-03-21 19:42:45 +0100

    Añadida tarea scripts a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp  = require('gulp');
 var sass  = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 
 gulp.task('styles', function(){
@@ -18,4 +21,14 @@ gulp.task('assets', function(){
 })
 
 
-gulp.task('default',['styles','assets']);
+gulp.task('scripts', function () {
+    browserify('./src/index.js')
+        .transform(babel, {presets: ["es2015"]})
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('default',['styles','assets','scripts']);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+alert('testing GULP + BROWSERIFY + BABEL + VINYL');
