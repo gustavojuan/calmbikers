4386eb1df39d5b10ee366929c18930f9041f4eae
gustavojuan <gustavojuan@gmail.com>
2017-03-21 19:17:37 +0100

    Assets con Gulp y favicon

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-
 var app = express();
 
 app.set('view engine','pug');
-
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -11,10 +9,7 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000, function(err){
-
     if(err) return console.log('Error'),process.exit(1);  
-
     console.log('listen port 3000');
-
 })
 
