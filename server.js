9aaf9a4fb49dc66cbe8e2bd91aad82808c446206
gustavojuan <gustavojuan@gmail.com>
2017-03-21 18:57:37 +0100

    Gulp sass

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine','pug');
+
+
+app.get('/', function(req, res){
+    res.render('index')
+});
+
+app.listen(3000, function(err){
+
+    if(err) return console.log('Error'),process.exit(1);  
+
+    console.log('listen port 3000');
+
+})
+
